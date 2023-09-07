a007c49dd7a6fa7cd8b3e8997efe13754261757f
Hulya <hulya.garibli@apertech.net>
2023-09-07 16:36:13 +0400

    gitignore added

---
+++ Warehouse.Core/Domain/Entities/ProductCustomer.cs
@@ -0,0 +1,14 @@
+﻿namespace Warehouse.Core.Domain.Entities
+{
+    public class ProductCustomer
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public decimal Quantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Status Status { get; set; }
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ Warehouse.Core/Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Warehouse.Core.Domain.Entities;
+
+namespace Warehouse.Core.Domain.Repositories
+{
+    public interface IProductRepository
+    {
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(int id);
+        Product Get(int id);
+        List<Product> GetAll();
+    }
+}
