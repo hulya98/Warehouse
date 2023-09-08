d98e49f3b643a9cc237f15e01b9f1a6674ae0e9f
sevinchuseynova <sevinc.huseynova@apertech.net>
2023-09-08 16:02:58 +0400

    Customer, mapper, icustomer, sqlcutomer added

--- Warehouse.Core/Domain/Entities/Customer.cs
+++ Warehouse.Core/Domain/Entities/Customer.cs
@@ -3,6 +3,6 @@
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string CustomerName { get; set; }
+        public string? CustomerName { get; set; }
     }
 }
---
+++ Warehouse.Core/Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.Core.Domain.Entities;
+
+namespace Warehouse.Core.Domain.Repositories
+{
+    internal interface ICustomerRepository
+    {
+        void Add(Customer customer);
+        void Delete(int CustomerId);
+        void Update(Customer customer);
+        Customer Get(int id);
+        List<Customer> Get();
+        List<Customer> Get();
+    }
+}
