942207f53388e4851d3eaf8fe2059174dad73154
Hulya <hulya.garibli@apertech.net>
2023-09-08 16:38:24 +0400

    bug fixed

--- Warehouse.Core/DataAccessLayer/SqlServer/SqlCustomerRepository.cs
+++ Warehouse.Core/DataAccessLayer/SqlServer/SqlCustomerRepository.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
